e3f3e2e08bc99b8f72073ccec1ad36cb0c8af2b6
Andrea Civita <andreacivita994@gmail.com>
2020-04-01 19:40:22 +0200

    first commit

---
+++ config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "title": "Vanda | Status Page",
+    "name": "vanda_or_status_page",
+    "description": "Welcome to Vanda's home for real-time and historical data on system performance.",
+    "defaultLocale": "it",
+    "locales": [
+        {
+            "code": "it",
+            "iso": "it-IT",
+            "name": "Italiano",
+            "file": "it.json"
+        }
+    ],
+    "content": {
+        "frontMatterFormat": "yaml",
+        "systems": [
+            "api",
+            "bot"
+        ],
+    },
+}
